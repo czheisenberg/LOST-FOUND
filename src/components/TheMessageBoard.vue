f6cc4ca7e497820211d41bb842698ae6db81ff64
czheisenberg <52897817+czheisenberg@users.noreply.github.com>
2024-07-07 17:39:21 +0800

    留言板后端server.js实现以及前端调用，留言数据库采用Notion database

--- src/components/TheMessageBoard.vue
+++ src/components/TheMessageBoard.vue
@@ -26,7 +26,7 @@
             :content="reply.content"
         />
       </ReplyContainer>
-      <ReplyBox @submit="addReply($event, comment.id)" />
+      <ReplyBox @submit="addNewComment($event, comment.id)" />
     </div>
   </div>
   <!-- 留言板 end -->
@@ -40,66 +40,67 @@ import CommentBox from "@/components/MessageBoardComponents/CommentBox.vue";
 import DividerHorizontal from "@/components/MessageBoardComponents/DividerHorizontal.vue";
 import CommentItem from "@/components/MessageBoardComponents/CommentItem.vue";
 import ReplyBox from "@/components/MessageBoardComponents/ReplyBox.vue";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import ReplyContainer from "@/components/MessageBoardComponents/ReplyContainer.vue";
 
 const face1 = ref('http://img-resource-198239.oss-cn-beijing.aliyuncs.com/images/c1c74ec5-3962-42b0-88ca-1a3e39eeb40f.jpg?Expires=1719907066&OSSAccessKeyId=LTAI5tQ6W9iAHKuWgLruyjwT&Signature=mWcyyCEQDLb2nPWLE%2FG7w41iMWY%3D')
 const face2 = ref('https://avatars.githubusercontent.com/u/52897817?v=4')
 const face3 = ref('http://img-resource-198239.oss-cn-beijing.aliyuncs.com/images/a7a69b93-128f-47b1-ad18-d8ad59808772.jpg?Expires=1719908331&OSSAccessKeyId=LTAI5tQ6W9iAHKuWgLruyjwT&Signature=vaRH3OdWy1OTiekpSQIMzYXg8CA%3D')
 const face4 = ref('')
-const comments = ref([
-  // {
-  //   id: 1,
-  //   user: "梦落轻寻",
-  //   avatar: face1.value,
-  //   time: "2小时之前",
-  //   content:
-  //       "哇！这篇文章真是写的太好啦！收到很大的启发，希望博主能够再接再厉，产出越来越多，越来越好的文章！凑字数，字数，字数...",
-  //   replies: [
-  //     {
-  //       id: 2,
-  //       user: "陌上花开",
-  //       avatar: face2.value,
-  //       time: "2小时之前",
-  //       content: "赞！",
-  //     },
-  //     {
-  //       id: 3,
-  //       user: "半梦半醒半浮生√<",
-  //       avatar: face3.value,
-  //       time: "2小时之前",
-  //       content:
-  //           "这是一篇非常长的长篇大论，这篇文章写的非常好，无论是技术点还是理论点，都非常的好。而且主题分明，每一个点都有自己的解释，这篇文章的主题是：CSS3的新特性，如何使用CSS3的新特性，以及如何使用CSS3的新特性。真的是非常好的文章。",
-  //     },
-  //   ],
-  // },
-]);
+const comments = ref([]);
+
+async function getAllComments() {
+  const res = await fetch("/api/comments");
+  comments.value = await res.json();
+}
 
 
-let rid = ref(4);
+// let rid = ref(4);
+//
+// const constructNewComment = (content) => {
+//   return {
+//     id: rid.value++,
+//     user: "当前用户",
+//     avatar: face4.value,
+//     content,
+//     time: "1秒前",
+//   };
+// };
+// const addNewComment = (content) => {
+//   const newComment = constructNewComment(content);
+//   comments.value.push(newComment);
+// };
+//
+// const addReply = (content, id) => {
+//   const reply = constructNewComment(content);
+//   let comment = comments.value.find((comment) => comment.id === id);
+//   if (comment.replies) {
+//     comment.replies.push(reply);
+//   } else {
+//     comment.replies = [reply];
+//   }
+// };
 
-const constructNewComment = (content) => {
-  return {
-    id: rid.value++,
-    user: "当前用户",
-    avatar: face4.value,
-    content,
-    time: "1秒前",
-  };
-};
-const addNewComment = (content) => {
-  const newComment = constructNewComment(content);
-  comments.value.push(newComment);
-};
+const addNewComment = async (content, replyTo) => {
+  await fetch(`/api/comments`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content,
+      ...(replyTo && { replyTo }),
+    }),
+  });
 
-const addReply = (content, id) => {
-  const reply = constructNewComment(content);
-  let comment = comments.value.find((comment) => comment.id === id);
-  if (comment.replies) {
-    comment.replies.push(reply);
-  } else {
-    comment.replies = [reply];
-  }
+  // 新增完评论后，自动获取新的评论列表
+  // Notion API 有延迟，在添加完 page 之后，需要过一会才能获取到新的评论列表
+  setTimeout(async () => {
+    await getAllComments();
+  }, 1000);
 };
 
+onMounted(()=>{
+  getAllComments();
+})
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // 替换为你的后端服务器地址
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }, // 如果需要重写路径，可以配置此项
+      },
+    },
+  },
 })
