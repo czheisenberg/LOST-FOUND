c4ff7191c37e7c373f286c86a7dc85b0169926e6
nx <18716734883a@gmail.com>
2024-07-11 19:45:10 +0800

    1.聊天集成完善

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -57,7 +57,7 @@
           <div class="mt-20">
             <router-link
                 class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                :to="{ name: 'chat', query: { userId: userId } }">
+                :to="{ name: 'chat', query: { userId: userId, username: username } }">
               与发布人聊天
             </router-link>
           </div>
